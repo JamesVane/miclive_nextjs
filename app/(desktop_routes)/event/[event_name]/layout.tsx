223c7897c90d062219c3d4eb92fbf37af926315d
JamesVane <jamesvanemusic@gmail.com>
2023-12-12 16:49:51 -0600

    foddling w layout more

--- app/(desktop_routes)/event/[event_name]/layout.tsx
+++ app/(desktop_routes)/event/[event_name]/layout.tsx
@@ -3,26 +3,28 @@
 import type { Metadata, ResolvingMetadata } from "next";
 import Head from "next/head";
 
-/* export const metadata: Metadata = {
-	title: "MIC.LIVE",
+export const metadata: Metadata = {
+	title: "TITLE!!!!!!",
 	description: "DESC",
-}; */
+	openGraph: {
+		description: "OG DESC!!!!!!",
+		title: "OG TITLE!!!!!!",
+	},
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
-			<Head>
-				<title>foofoo</title>
-				<meta
-					name="title"
-					content="Next.js 13 - Layouts and Pages Explained in 20 mins or so"
-				/>
-				<meta
-					property="og:description"
-					content="And a social description for our cool page"
-				/>
-				<meta property="og:title" content="Social Title for Cool Page" />
-			</Head>
+			{/* <title>foofoo</title>
+			<meta
+				name="title"
+				content="Next.js 13 - Layouts and Pages Explained in 20 mins or so"
+			/>
+			<meta
+				property="og:description"
+				content="And a social description for our cool page"
+			/>
+			<meta property="og:title" content="Social Title for Cool Page" /> */}
 
 			<main>{children}</main>
 		</>
