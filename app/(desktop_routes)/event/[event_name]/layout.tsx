1fdeb1ab369df28b10bd4eb5a10bf32302db4344
JamesVane <jamesvanemusic@gmail.com>
2023-12-12 16:06:35 -0600

    foddling w layout

---
+++ app/(desktop_routes)/event/[event_name]/layout.tsx
@@ -0,0 +1,21 @@
+/** @format */
+
+import type { Metadata, ResolvingMetadata } from "next";
+
+/* export const metadata: Metadata = {
+	title: "MIC.LIVE",
+	description: "DESC",
+}; */
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			<meta
+				name="title"
+				content="Next.js 13 - Layouts and Pages Explained in 20 mins or so"
+			/>
+			<title>foofoo</title>
+			<main>{children}</main>
+		</>
+	);
+}
