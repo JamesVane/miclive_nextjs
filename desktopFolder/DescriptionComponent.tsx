a5875207670ebb60bc9438d3266b8a6fdf2e3c38
JamesVane <jamesvanemusic@gmail.com>
2023-12-13 14:22:49 -0600

    hopeflly fix

--- desktopFolder/DescriptionComponent.tsx
+++ desktopFolder/DescriptionComponent.tsx
@@ -5,16 +5,16 @@ import React from "react";
 import ReactQuill from "react-quill";
 
 function DescriptionComponent({ text }: { text: string }) {
-	return (
-		<ReactQuill
+	return {
+		/* <ReactQuill
 			value={JSON.parse(text)}
 			readOnly={true}
 			theme="bubble"
 			style={{
 				width: "100%",
 			}}
-		/>
-	);
+		/> */
+	};
 }
 
 export default DescriptionComponent;
